e410fff30ecf15c97f233c1c02e81db19382da5d
Shrinithi123 <34883894+Shrinithi123@users.noreply.github.com>
2021-07-24 06:27:09 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -19,7 +19,7 @@ function preload(){
 }
 
 function setup(){
-  canvas = createCanvas(displayWidth - 20, displayHeight-30);
+  canvas = createCanvas(displayWidth - 21, displayHeight-30);
   database = firebase.database();
   game = new Game();
   game.getState();
